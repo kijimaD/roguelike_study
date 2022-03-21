184089d8f257d944cb47ecb1970f0da9a3142ee4
kijimaD <norimaking777@gmail.com>
2022-03-21 19:01:36 +0900

    feat: monsters no longer end up on top of each other

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,8 @@ mod monster_ai_system;
 pub use monster_ai_system::*;
 mod visibility_system;
 use visibility_system::VisibilitySystem;
+mod map_indexing_system;
+use map_indexing_system::MapIndexingSystem;
 
 #[derive(PartialEq, Copy, Clone)]
 pub enum RunState { Paused, Running }
@@ -27,6 +29,8 @@ impl State {
         vis.run_now(&self.ecs);
         let mut mob = MonsterAI{};
         mob.run_now(&self.ecs);
+        let mut mapindex = MapIndexingSystem{};
+        mapindex.run_now(&self.ecs);
         self.ecs.maintain();
     }
 }
@@ -71,6 +75,7 @@ fn main() -> rltk::BError {
     gs.ecs.register::<Monster>();
     gs.ecs.register::<Viewshed>();
     gs.ecs.register::<Name>();
+    gs.ecs.register::<BlocksTile>();
 
     let map : Map = Map::new_map_rooms_and_corridors();
     let (player_x, player_y) = map.rooms[0].center();
@@ -98,6 +103,7 @@ fn main() -> rltk::BError {
             .with(Viewshed{ visible_tiles : Vec::new(), range: 8, dirty: true })
             .with(Monster{})
             .with(Name{ name: format!("{} #{}", &name, i)})
+            .with(BlocksTile{})
             .build();
     }
 
