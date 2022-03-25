af4be7910684d42de91a2935a971fd739fa97172
kijimaD <norimaking777@gmail.com>
2022-03-26 00:00:27 +0900

    refactor: divide function

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use rltk::{GameState, Rltk, RGB, Point};
+use rltk::{GameState, Rltk, Point};
 use specs::prelude::*;
 mod components;
 pub use components::*;
@@ -120,31 +120,10 @@ fn main() -> rltk::BError {
 
     let player_entity = spawner::player(&mut gs.ecs, player_x, player_y);
 
-    let mut rng = rltk::RandomNumberGenerator::new();
-    for (i,room) in map.rooms.iter().skip(1).enumerate() {
-        let (x,y) = room.center();
-
-        let glyph : rltk::FontCharType;
-        let name : String;
-        let roll = rng.roll_dice(1, 2);
-        match roll {
-            1 => { glyph = rltk::to_cp437('g'); name = "Goblin".to_string(); }
-            _ => { glyph = rltk::to_cp437('o'); name = "Orc".to_string(); }
-        }
-
-        gs.ecs.create_entity()
-            .with(Position{ x, y })
-            .with(Renderable{
-                glyph,
-                fg: RGB::named(rltk::RED),
-                bg: RGB::named(rltk::BLACK),
-            })
-            .with(Viewshed{ visible_tiles : Vec::new(), range: 8, dirty: true })
-            .with(Monster{})
-            .with(Name{ name: format!("{} #{}", &name, i) })
-            .with(BlocksTile{})
-            .with(CombatStats{ max_hp: 16, hp: 16, defense: 1, power: 4 })
-            .build();
+    gs.ecs.insert(rltk::RandomNumberGenerator::new());
+    for room in map.rooms.iter().skip(1) {
+        let (x, y) = room.center();
+        spawner::random_monster(&mut gs.ecs, x, y)
     }
 
     gs.ecs.insert(map);
